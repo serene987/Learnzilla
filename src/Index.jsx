1eb309d87e8284603e116f8969601b3f689a21b3
serene987 <serena.pereira21@gamil.com>
2025-03-06 13:32:16 +0530

    Index pg : version 0.5 (updates)

--- src/Index.jsx
+++ src/Index.jsx
@@ -2,6 +2,8 @@ import styles from './index.module.css'
 // import Dropdown from 'react-bootstrap/Dropdown';
 // import { NavLink } from 'react-bootstrap';
 
+// import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
 export default function Index(){
 
     return(
@@ -19,8 +21,11 @@ export default function Index(){
                         <li>
                             <a href='#'>Services</a>
                         </li>
-                        <li className={styles.signIn}>
-                            <a href='#'>Sign in</a>
+                    </ul>
+                    <ul className={styles.signIn}>
+                        {/* on click of sign in, card opens with student or teacher option */}
+                        <li>
+                            <AiIcons.AiOutlineUser/> <a href='#'>Sign in</a>
                         </li>
                     </ul>
                 </nav>
