6852175bddf5f5adf558b9bb4f8fd3ffc6ffc425
serene987 <serena.pereira21@gamil.com>
2025-03-08 23:58:54 +0530

    reset the deleted files 1.3

---
+++ src/pages/teacher/Myclasses.jsx
@@ -0,0 +1,10 @@
+
+function Myclasses() {
+    return (
+      <div className='Myclasses'>
+        <h1>Myclasses</h1>
+      </div>
+    );
+  }
+  
+  export default Myclasses;
---
+++ src/pages/teacher/Sidebar.jsx
@@ -0,0 +1,10 @@
+
+function Sidebar() {
+    return (
+      <div className='Sidebar'>
+        <h1>Sidebar</h1>
+      </div>
+    );
+  }
+  
+  export default Sidebar;
