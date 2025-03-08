8e37f3200775cbba8e2669b3ad4303b26da05674
serene987 <serena.pereira21@gamil.com>
2025-03-09 00:06:33 +0530

    reset the deleted files 1.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import './App.css';
 import Index from './Index';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/student/Home.jsx'
+import Classes from './pages/student/Classes.jsx'
+import ClassDetails from './pages/student/ClassDetails.jsx'
+import Support from './pages/student/Support.jsx'
+import Attendance from './pages/student/Attendance.jsx'
 
 // import Topnav from './components/topnav/Topnav.jsx'
 
@@ -8,6 +14,15 @@ import Index from './Index';
 function App() {
   return (
     <>
+      <div>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/classes' element={<Classes/>}/>
+          <Route path='/class/:id' element={<ClassDetails/>}/>
+          <Route path='/support' element={<Support/>}/>
+          <Route path='/attendance' element={<Attendance/>}/>
+        </Routes>
+      </div>
       <Index/>
     </>
   );
