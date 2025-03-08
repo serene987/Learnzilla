f67068c977f5b5ddc7339acec67ca4490e46e8c6
serene987 <serena.pereira21@gamil.com>
2025-03-08 23:48:48 +0530

    reset the deleted files 1.1

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createContext } from 'react-dom'
+import { AppContext } from './context/AppContext.jsx'
 // import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <AppContext>
     <App />
-  </StrictMode>,
+  </AppContext>,
 )
