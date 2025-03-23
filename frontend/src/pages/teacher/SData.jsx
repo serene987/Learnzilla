504ebf402398100c6892ac21b18809b9552dff54
serene987 <serena.pereira21@gamil.com>
2025-03-23 22:02:17 +0530

    teacherDash : version 3.2 (teacher pgs)

--- frontend/src/pages/teacher/SData.jsx
+++ frontend/src/pages/teacher/SData.jsx
@@ -8,30 +8,28 @@ export const SData = [
     title: 'Home',
     path: '/',
     icon: <AiIcons.AiFillHome />,
-    cName: 'nav-text'
   },
   {
     title: 'My classes',
     path: '/myclasses',
     icon: <IoIcons.IoIosPaper />,
-    cName: 'nav-text'
   },
   {
-    title: 'Attendance',
+    title: 'Mark Attendance',
     path: '/attendance',
     icon: <FaIcons.FaEnvelopeOpenText />,
-    cName: 'nav-text'
+
   },
   {
     title: 'My Students',
     path: '/mystudents',
     icon: <PiIcons.PiStudentBold/>,
-    cName: 'nav-text'
+
   },
   {
     title: 'Support',
     path: '/support',
     icon: <IoIcons.IoMdHelpCircle />,
-    cName: 'nav-text'
+
   }
 ];
