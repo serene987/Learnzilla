e1d84d4059b958dd1efa43e0004651074692e371
serene987 <serena.pereira21@gamil.com>
2025-04-01 02:29:28 +0530

    student :verion 2

---
+++ frontend/src/pages/student/NavbarData.jsx
@@ -0,0 +1,17 @@
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+import * as PiIcons from "react-icons/pi";
+
+export const SData = [
+  {
+    title: 'Home',
+    path: '/myhome',
+    icon: <AiIcons.AiFillHome />,
+  },
+  {
+    title: 'My Grades',
+    path: '/mygrades',
+    icon: <IoIcons.IoIosPaper />,
+  },
+];
