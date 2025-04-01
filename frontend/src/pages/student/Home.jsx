5ec46c81e8814c6722ce2a2d3fe5984ac061dff7
serene987 <serena.pereira21@gamil.com>
2025-04-01 13:56:33 +0530

    home.jsx error commented

--- frontend/src/pages/student/Home.jsx
+++ frontend/src/pages/student/Home.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import Navbar from "../../components/StudentNavbar.jsx";
 import Card from "../../components/Card.jsx";
 
@@ -7,16 +6,16 @@ import StudentGrades from "../../components/Grades.jsx";
 import StudentAnnouncements from "../../components/Announcement.jsx";
 import StudentAttendance from '../../components/Myattendance.jsx' 
 
-const courses = [
-  { title: "XII", course: "Science", teacher: "Mr. Sharma", backgroundImage: "science.jpg" },
-  { title: "XI", course: "Commerce", teacher: "Mrs. Patel", backgroundImage: "commerce.jpg" },
-  { title: "X", course: "Arts", teacher: "Ms. D’Souza", backgroundImage: "arts.jpg" },
-  { title: "IX", course: "Maths", teacher: "Mr. Verma", backgroundImage: "math.jpg" },
-  { title: "VIII", course: "History", teacher: "Mrs. Mehta", backgroundImage: "history.jpg" },
-];
+// const courses = [
+//   { title: "XII", course: "Science", teacher: "Mr. Sharma", backgroundImage: "science.jpg" },
+//   { title: "XI", course: "Commerce", teacher: "Mrs. Patel", backgroundImage: "commerce.jpg" },
+//   { title: "X", course: "Arts", teacher: "Ms. D’Souza", backgroundImage: "arts.jpg" },
+//   { title: "IX", course: "Maths", teacher: "Mr. Verma", backgroundImage: "math.jpg" },
+//   { title: "VIII", course: "History", teacher: "Mrs. Mehta", backgroundImage: "history.jpg" },
+// ];
 
 const Home = () => {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
+  // const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
     <>
